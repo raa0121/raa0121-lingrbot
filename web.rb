33ed9cd4cfe04617e65bac42217dfcedf26917f9
raa0121 <raa0121@gmail.com>
2012-01-19 16:55:15 +0900

    del require Random

--- web.rb
+++ web.rb
@@ -1,20 +1,18 @@
 require 'sinatra'
 require 'json'
-require 'Random'
 
 get '/' do
 	'lingr:DiceBot'
 end
 post '/'do
 	json = JSON.parce(parms[:json])
-	rnd = Random.new
 	tmp = ""
 	json["events"].each do |e|
 	if e["message"]
 		m = e["message"]["text"]
 		if /^(\d*)d(\d*)/ =~ m
 			$1.times do 
-				tmp = rnd.rand($2.to_i -1)+1
+				tmp = rand($2.to_i -1)+1
 				tmp += " "
 			end
 		end
