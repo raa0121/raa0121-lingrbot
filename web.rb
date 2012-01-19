9609006b47c7b2d60027744d01c31ba5dfd7fe73
raa0121 <raa0121@gmail.com>
2012-01-19 17:18:17 +0900

    post your post

--- web.rb
+++ web.rb
@@ -8,13 +8,14 @@ post '/'do
 	json = JSON.parse(request.env['rack.input'].read)
 	tmp = []
 	json["events"].each do |e|
-	if e["message"]
-		m = e["message"]["text"]
-		if /^(\d*)d(\d*)/ =~ m
-			$1.to_i.times do |i| 
-				tmp[i] = rand($2.to_i -1)+1
-			end
-		end
-	end
-	tmp.join ","
+		request.env['rack.input'].read
+	#if e["message"]
+	#	m = e["message"]["text"]
+	#	if /^(\d*)d(\d*)/ =~ m
+	#		$1.to_i.times do |i| 
+	#			tmp[i] = rand($2.to_i -1)+1
+	#		end
+	#	end
+	#end
+	#tmp.join ","
 end
