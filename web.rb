5ce5bd5a042c5bfad0c7bbd008744b1428783196
raa0121 <raa0121@gmail.com>
2012-01-19 17:08:28 +0900

    tmp is changed to array

--- web.rb
+++ web.rb
@@ -5,17 +5,16 @@ get '/' do
 	'lingr:DiceBot'
 end
 post '/'do
-	json = JSON.parce(parms[:json])
-	tmp = ""
+	json = JSON.parse(params[:json])
+	tmp = []
 	json["events"].each do |e|
 	if e["message"]
 		m = e["message"]["text"]
 		if /^(\d*)d(\d*)/ =~ m
-			$1.times do 
-				tmp = rand($2.to_i -1)+1
-				tmp += " "
+			$1.to_i.times do |i| 
+				tmp[i] = rand($2.to_i -1)+1
 			end
 		end
 	end
-	tmp
+	tmp.join ","
 end
