0601be3b90e26b5759fd52217287223d13902216
raa0121 <raa0121@gmail.com>
2012-01-19 15:07:11 +0900

    test

--- web.rb
+++ web.rb
@@ -5,10 +5,10 @@ require "cgi"
 json = JSON.parse(CGI.new["json"])
 
 get '/' do
-	"hello world"
-	if /(^d*)d(^d*)/ =~ json["events"]["message"]["text"] do
-		print "Content-Type: text/plain\n\n"
-		$1.times do
-				print rand($2-1)+1
-	end
+	'hello world'
+	#if /(^d*)d(^d*)/ =~ json["events"]["message"]["text"] do
+	#	print "Content-Type: text/plain\n\n"
+	#	$1.times do
+	#			print rand($2-1)+1
+	#end
 end
