4246aab931b30b66fbbb55f1050b4b882173cd10
raa0121 <raa0121@gmail.com>
2012-01-19 15:52:40 +0900

    cgi test2

--- web.rb
+++ web.rb
@@ -1,14 +1,14 @@
 require 'sinatra'
-#require "json"
-#require "cgi"
+require "json"
+require "cgi"
 
-#json = JSON.parse(CGI.new["json"])
+json = JSON.parse(CGI.new["json"])
 
 get '/' do
 	'hello world'
-	#if /(^d*)d(^d*)/ =~ json["events"]["message"]["text"] do
-	#	print "Content-Type: text/plain\n\n"
-	#	$1.times do
-	#			print rand($2-1)+1
-	#end
+	if /(^d*)d(^d*)/ =~ json["events"]["message"]["text"] 
+		print "Content-Type: text/plain\n\n"
+		$1.times do
+				print rand($2-1)+1
+	end
 end
