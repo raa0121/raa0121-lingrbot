87ba58dc12dd87deaf502ca5d470bfa2e7f11e34
raa0121 <raa0121@gmail.com>
2012-09-25 10:54:18 +0900

    command change !ruby 2

--- bot.rb
+++ bot.rb
@@ -10,7 +10,7 @@ post '/ruby' do
   json["events"].map do |e|
     if e["message"]
       m = e["message"]["text"]
-      if /^!ruby\s(.*)/ =~ m
+      if /^!ruby\s+(.*)/m =~ m
         #x = Thread.start do
         $SAFE = 2
         after = eval "#{$1}"
