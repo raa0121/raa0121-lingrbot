6462ab583b640bb0e2dfe57822f492f353a73c8b
ujihisa <ujihisa@gmail.com>
2013-01-24 20:37:33 -0800

    follows convention. gameType -> game_type

--- dice.rb
+++ dice.rb
@@ -11,7 +11,7 @@ GAMELIST = File.read("gameList.json")
 post '/dice' do
   content_type :text
   json = JSON.parse(request.body.string)
-  gameType = '""'
+  game_type = '""'
 
   json["events"].select {|e|
     e["message"]
@@ -21,8 +21,8 @@ post '/dice' do
     command = m.strip.split(/[\s　]/)
     diceCommand = command[0].gsub(">","\\>").gsub("<","\\<").gsub("(","\\(").gsub(")","\\)").gsub("=","\\=")
     gameList = JSON.parse(GAMELIST)
-    gameType = gameList.fetch(command[1], '""')
-    result = `cd #{BCDicePATH}; ruby customDiceBot.rb #{diceCommand} #{gameType}`
+    game_type = gameList.fetch(command[1], '""')
+    result = `cd #{BCDicePATH}; ruby customDiceBot.rb #{diceCommand} #{game_type}`
     "#{u} : #{result.gsub("\n","")}" unless result == "\n"
   }.join
 end
