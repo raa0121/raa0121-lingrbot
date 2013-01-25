2927e9609fe4de2a6bba4341123cea9e6e6f211f
ujihisa <ujihisa@gmail.com>
2013-01-24 20:25:40 -0800

    indentation

--- dice.rb
+++ dice.rb
@@ -14,15 +14,15 @@ post '/dice' do
   json["events"].select {|e|
     e["message"]
   }.map {|e|
-      m = e["message"]["text"]
-      u = e["message"]["nickname"]
-      command = m.strip.split(/[\s　]/)
-      diceCommand = command[0].gsub(">","\\>").gsub("<","\\<").gsub("(","\\(").gsub(")","\\)").gsub("=","\\=")
-      File.open("gameList.json","r"){|gl|
-        gameList = JSON.parse(gl.read)
-        gameType = gameList.fetch(command[1],"\"\"")
-        result = `cd #{BCDicePATH}; ruby customDiceBot.rb #{diceCommand} #{gameType}`
-        "#{u} : #{result.gsub("\n","")}" unless result == "\n"
-      }
+    m = e["message"]["text"]
+    u = e["message"]["nickname"]
+    command = m.strip.split(/[\s　]/)
+    diceCommand = command[0].gsub(">","\\>").gsub("<","\\<").gsub("(","\\(").gsub(")","\\)").gsub("=","\\=")
+    File.open("gameList.json","r"){|gl|
+      gameList = JSON.parse(gl.read)
+      gameType = gameList.fetch(command[1],"\"\"")
+      result = `cd #{BCDicePATH}; ruby customDiceBot.rb #{diceCommand} #{gameType}`
+      "#{u} : #{result.gsub("\n","")}" unless result == "\n"
+    }
   }.join
 end
