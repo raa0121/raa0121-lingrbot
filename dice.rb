e349b54cde192c3d12f9dc851d0d7e74bb64ad4d
ujihisa <ujihisa@gmail.com>
2013-01-24 20:36:16 -0800

    Revert "equivalent"
    This reverts commit fa2892414095e17326336ba9b05f540e1f1a3f64.

--- dice.rb
+++ dice.rb
@@ -18,7 +18,7 @@ post '/dice' do
   }.map {|e|
     m = e["message"]["text"]
     u = e["message"]["nickname"]
-    command = m.strip.split(/\s/)
+    command = m.strip.split(/[\s　]/)
     diceCommand = command[0].gsub(">","\\>").gsub("<","\\<").gsub("(","\\(").gsub(")","\\)").gsub("=","\\=")
     gameList = JSON.parse(GAMELIST)
     gameType = gameList.fetch(command[1], '""')
