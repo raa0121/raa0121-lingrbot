8e9c08245bc67ac1449a99c1f3429e2e9cbca1c6
raa0121 <raa0121@gmail.com>
2013-02-27 18:56:58 +0900

    add ja-doc2

--- vim.rb
+++ vim.rb
@@ -51,8 +51,9 @@ post '/vim' do
         if help[1] =~ /@ja/
           tags = jatags
           docroot = jadocroot
+          t = tags.select {|t| t[0] == help[1]}.first
+          t[2].sub! /.txt$/, 'jax'
         end
-        t = tags.select {|t| t[0] == help[1]}.first
         if t
           text = open("#{docroot}/#{t[1]}").read
           text = text[/^.*(?:\s+\*[^\n\s]+\*)*\s#{Regexp.escape(t[2][1..-1])}(?:\s+\*[^\n\s]+\*)*$/.match(text).begin(0)..-1]
