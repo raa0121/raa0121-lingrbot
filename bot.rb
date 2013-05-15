fe7697ad204461010811d4525a5468ee949b2534
raa0121 <raa0121@gmail.com>
2013-05-15 15:38:53 +0900

    \@

--- bot.rb
+++ bot.rb
@@ -9,9 +9,10 @@ post '/ruby' do
   content_type :text
   json = JSON.parse(request.body.string)
   json["events"].map do |e|
-    if e["message"]
-      m = e["message"]["text"]
-      if /^!(c)?ruby\s+(.*)/m =~ m
+    @m = e["message"]
+    if @m
+      t = @m["text"]
+      if /^!(c)?ruby\s+(.*)/m =~ t
         #x = Thread.start do
         $SAFE = 2
         after = eval "#{$2}"
