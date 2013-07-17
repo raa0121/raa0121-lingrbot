75aacb2d37ecded883c08f74dd54a513386ac393
Code Ass <aycabta@gmail.com>
2013-07-18 00:08:35 +0900

    support twitpic

--- nicothumb.rb
+++ nicothumb.rb
@@ -57,6 +57,10 @@ post '/nicothumb' do
         url = `./gyazo ameba_#{file}.#{type}`.gsub("\n","")
         File.delete("ameba_#{file}.#{type}")
         "#{url.sub("//","//cache.")}.png"
+      elsif /^http:\/\/twitpic.com\/[0-9a-z]+/ =~ m
+        agent.get("#{m}/full")
+        twitpic = agent.page.parser.xpath("//div[@id='media-full']/img").first.attributes["src"].value
+        twitpic
       end
     end
   }
