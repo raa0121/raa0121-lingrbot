4c1509f89bc8fa220f122c24655e55b05720c18b
raa0121 <raa0121@gmail.com>
2013-12-20 15:46:11 +0900

    foget year

--- vim.rb
+++ vim.rb
@@ -46,7 +46,7 @@ def VimAdv(event, year)
   if "13" == year
     atnd_url = "http://atnd.org/events/45072"
     descript = $VAC13.split("\n")
-  elsif "12"
+  elsif "12" == year
     atnd_url = "http://atnd.org/events/33746"
     descript = $VAC12.split("\n")
   else
