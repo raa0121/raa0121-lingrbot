313093daae514a6f8e7a63cbf9ea8524db0bffe6
raa0121 <raa0121@gmail.com>
2014-01-23 00:47:35 +0900

    fixfix

--- lyrics.rb
+++ lyrics.rb
@@ -12,7 +12,7 @@ def searchMusic(word)
   return "http://www.utamap.com/phpflash/flashfalsephp.php?unum=#{id}"
 end
 def getLyric(mes)
-  command = mes.strip.split(/[\s　]/)
+  command = mes.sub("!lyrics","").strip
   lyric_url = searchMusic(command[1])
   lyric = open(lyric_url).read.force_encoding("utf-8").sub(/test1=\d+&test2=/,"")
 end
