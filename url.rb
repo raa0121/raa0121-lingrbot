9923fc2226acb56bc1ed6cdb4d88602e9d2adaed
raa0121 <raa0121@gmail.com>
2014-02-20 10:55:01 +0900

    add url

---
+++ url.rb
@@ -0,0 +1,15 @@
+require 'mechanize'
+
+@agent = Mechanize.new
+
+get '/url' do
+  content_type :text
+  json = JSON.parse(request.body.read)
+  json["events"].select {|e| e['message'] }.map {|e|
+    m = e["message"]["text"]
+    if /^https?:\/\/.*/ =~ m
+      @agent.get(m)
+      @agent.page.at('title').inner_text
+    end
+  }
+end
