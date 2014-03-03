b4395fb649b797faa7db7a9f3fd5c337707140a6
raa0121 <raa0121@gmail.com>
2014-03-03 19:03:10 +0900

    if "" == lyric_url

--- lyrics.rb
+++ lyrics.rb
@@ -40,6 +40,9 @@ end
 def getLyric(mes,room)
   command = mes.sub("!lyrics","").strip
   lyric_url = searchMusic(command)
+  if "" == lyric_url
+    return ""
+  end
   begin
     lyric_page = $agent.get(lyric_url)
     lyric = $agent.page('p').text.sub(/test1=\d+&test2=/,"")
