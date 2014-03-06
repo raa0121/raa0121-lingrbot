0a7a5ff1a72e49e8a4d493e20f4a1752c63efab0
raa0121 <raa0121@gmail.com>
2014-03-06 21:52:46 +0900

    fix

--- vim.rb
+++ vim.rb
@@ -47,13 +47,14 @@ def VimAdv(event, year)
   descript = []
   if "13" == year
     atnd_url = "http://atnd.org/events/45072"
-    descript = $VAC13.split("\n")
+    descript = $VAC13["events"][0]["description"].split("\r\n")
+
   elsif "12" == year
     atnd_url = "http://atnd.org/events/33746"
-    descript = $VAC12.split("\n")
+    descript = $VAC12["events"][0]["description"].split("\r\n")
   else
     atnd_url = "http://atnd.org/events/45072 & http://atnd.org/events/33746"
-    descript = $VAC12.split("\n")
+    descript = $VAC12["events"][0]["description"].split("\r\n")
   end
   data = Hash.new
   user = [];search=[]
