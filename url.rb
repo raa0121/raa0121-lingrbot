1584a660f6a32f816becf8a940a302b589aceb31
raa0121 <raa0121@gmail.com>
2014-03-21 19:32:02 +0900

    URI.extract

--- url.rb
+++ url.rb
@@ -1,6 +1,7 @@
 require 'mechanize'
 require 'cgi'
 require 'json'
+require 'uri'
 
 $agent = Mechanize.new
 $agent.user_agent = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0"
@@ -8,30 +9,33 @@ $agent.user_agent = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:26.0) Gecko/20100101
 post '/url' do
   content_type :text
   json = JSON.parse(request.body.read)
+  titles = []
   json["events"].select {|e| e['message'] }.map {|e|
     m = e["message"]["text"]
-    if /(https?:\/\/\S+)/ =~ m
-      url = $1
-      begin
-        unless Mechanize::Page == $agent.get(url).class
-          return ""
-        end
-        if %r`\Ahttps?://(www\.)?twitter.com/[^/]+/status/(\d+)` =~ url
-          tweet = $agent.page.at("[data-tweet-id='#{$2}']")
-          return 'Something wrong with twitter url.' unless tweet
-          return "#{tweet.at('strong.fullname').inner_text} / #{tweet.at('span.js-action-profile-name').inner_text}\n#{tweet.at('p.tweet-text').inner_text}"
-        end
-        CGI.unescapeHTML($agent.page.at('title').inner_text)
-      rescue Mechanize::ResponseCodeError => ex
-        case ex.response_code
-        when '403'
-          return ""
-        when '404'
-          return ""
-        when '503'
-          return ""
-        when '500'
-          return ""
+    unless [] == urls = URI.extract(m, ["http", "https"])
+      urls.each do |url|
+        begin
+          unless Mechanize::Page == $agent.get(url).class
+            return ""
+          end
+          if %r`\Ahttps?://(www\.)?twitter.com/[^/]+/status/(\d+)` =~ url
+            tweet = $agent.page.at("[data-tweet-id='#{$2}']")
+            titles << 'Something wrong with twitter url.' unless tweet
+            titles << "#{tweet.at('strong.fullname').inner_text} / #{tweet.at('span.js-action-profile-name').inner_text}\n#{tweet.at('p.tweet-text').inner_text}"
+          end
+          titles << CGI.unescapeHTML($agent.page.at('title').inner_text)
+          titles.join("\n")
+        rescue Mechanize::ResponseCodeError => ex
+          case ex.response_code
+          when '403'
+            return ""
+          when '404'
+            return ""
+          when '503'
+            return ""
+          when '500'
+            return ""
+          end
         end
       end
     end
