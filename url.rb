4d178f248591598e63fecb41ddb49ba79bda97e6
raa0121 <raa0121@gmail.com>
2014-03-21 20:02:20 +0900

    fix

--- url.rb
+++ url.rb
@@ -24,7 +24,6 @@ post '/url' do
             titles << "#{tweet.at('strong.fullname').inner_text} / #{tweet.at('span.js-action-profile-name').inner_text}\n#{tweet.at('p.tweet-text').inner_text}"
           end
           titles << CGI.unescapeHTML($agent.page.at('title').inner_text)
-          return titles.join("\n")
         rescue Mechanize::ResponseCodeError => ex
           case ex.response_code
           when '403'
@@ -38,6 +37,7 @@ post '/url' do
           end
         end
       end
+      return titles.join("\n")
     end
   }
 end
