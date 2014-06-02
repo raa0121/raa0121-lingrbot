345a08f32efb07b17054dd26354689fcb58ff39a
Code Ass <aycabta@gmail.com>
2014-06-02 17:52:10 +0900

    Revert "move Gyazo to direct link from URL bot to thumb bot"
    This reverts commit 301108f95475f81a9c2aff7edc3224646971dbd6.

--- url.rb
+++ url.rb
@@ -17,6 +17,9 @@ post '/url' do
           case $agent.get(url)
           when Mechanize::Page, Mechanize::XmlFile
             case url
+            when %r`\Ahttp://gyazo\.com/(\w+)`
+              gyazo_raw_url = "http://i.gyazo.com/#{$1}.png"
+              response_lines << gyazo_raw_url
             when %r`\Ahttps?://(www\.)?twitter.com/[^/]+/status/(\d+)`
               tweet = $agent.page.at("[data-tweet-id='#{$2}']")
               response_lines << 'Something wrong with twitter url.' unless tweet
